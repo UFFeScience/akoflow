1fc2cab0401e5f063c7f1d414c91e0035f30e793
Wesley <wesley.ferreira@businessmonster.com.br>
2024-03-11 20:04:37 -0300

    client: send file to server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"flag"
+	"github.com/ovvesley/scientific-workflow-k8s/pkg/client/services/dispatch_to_server_run_workflow_service"
+)
+
 func main() {
+	host := flag.String("host", "localhost", "host")
+	port := flag.String("port", "8080", "port")
+	fileYaml := flag.String("file", "", "file")
+
+	flag.Parse()
+
+	if *fileYaml == "" {
+		println("file is required")
+		return
+	}
+
+	dispatchToServerRunWorkflowService := dispatch_to_server_run_workflow_service.New()
+
+	dispatchToServerRunWorkflowService.SetHost(*host)
+	dispatchToServerRunWorkflowService.SetPort(*port)
+	dispatchToServerRunWorkflowService.SetFile(*fileYaml)
+
+	dispatchToServerRunWorkflowService.Run()
 
 }
---
+++ pkg/client/services/dispatch_to_server_run_workflow_service/dispatch_to_server_run_workflow_service.go
@@ -0,0 +1,143 @@
+package dispatch_to_server_run_workflow_service
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+type DispatchToServerRunWorkflowService struct {
+	host string
+	port string
+	file string
+}
+
+type RequestPostRunWorkflow struct {
+	Workflow string `json:"workflow"`
+}
+
+type ResponsePostRunWorkflow struct {
+	Workflow string `json:"workflow"`
+	Message  string `json:"message"`
+}
+
+func New() *DispatchToServerRunWorkflowService {
+	return &DispatchToServerRunWorkflowService{}
+}
+
+func (d *DispatchToServerRunWorkflowService) SetHost(host string) *DispatchToServerRunWorkflowService {
+	d.host = host
+	return d
+}
+
+func (d *DispatchToServerRunWorkflowService) SetPort(port string) *DispatchToServerRunWorkflowService {
+	d.port = port
+	return d
+}
+
+func (d *DispatchToServerRunWorkflowService) SetFile(file string) *DispatchToServerRunWorkflowService {
+	d.file = file
+	return d
+}
+
+func (d *DispatchToServerRunWorkflowService) GetHost() string {
+	return d.host
+}
+
+func (d *DispatchToServerRunWorkflowService) GetPort() string {
+	return d.port
+}
+
+func (d *DispatchToServerRunWorkflowService) GetFile() string {
+	return d.file
+}
+
+func (d *DispatchToServerRunWorkflowService) Run() {
+
+	println("host:", d.host)
+	println("port:", d.port)
+	println("file:", d.file)
+
+	// read file
+
+	base64FileContent := d.getBase64FileContent(d.GetFile())
+
+	d.sendToServer(base64FileContent)
+
+}
+
+func (d *DispatchToServerRunWorkflowService) getBase64FileContent(filePath string) string {
+	fileContent := d.getFileContent(filePath)
+
+	base64FileContent := base64.StdEncoding.EncodeToString([]byte(fileContent))
+	return base64FileContent
+}
+
+func (d *DispatchToServerRunWorkflowService) getFileContent(filePath string) string {
+	// read file
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+		return ""
+	}
+
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		panic(err)
+		return ""
+	}
+
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		panic(err)
+		return ""
+	}
+
+	return string(bs)
+
+}
+
+func (d *DispatchToServerRunWorkflowService) sendToServer(base64FileContent string) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+
+	payload := RequestPostRunWorkflow{
+		Workflow: base64FileContent,
+	}
+
+	payloadJson, _ := json.Marshal(payload)
+
+	req, _ := http.NewRequest("POST", "http://"+d.GetHost()+":"+d.GetPort()+"/runworkflow", bytes.NewBuffer(payloadJson))
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		println("error:", err)
+	}
+
+	defer resp.Body.Close()
+
+	var result ResponsePostRunWorkflow
+	err = json.NewDecoder(resp.Body).Decode(&result)
+
+	if err != nil {
+		println("error:", err)
+	}
+
+	println("workflow:", result.Workflow)
+	println("message:", result.Message)
+
+}

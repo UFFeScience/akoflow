1b13f3b866739f2982aa3352f0573699cd45d733
Wesley <wesley.ferreira@businessmonster.com.br>
2024-03-27 09:47:31 -0300

    scik8sflow: storage path to sqlite

--- pkg/server/repository/repository.go
+++ pkg/server/repository/repository.go
@@ -3,15 +3,19 @@ package repository
 import (
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"os"
 )
 
 type Database struct {
 	file string
 }
 
-const File = "db.sqlite"
+const File = "storage/database.db"
 
 func (d *Database) Connect() *sql.DB {
+
+	createDirectoryIfNotExists("storage")
+
 	db, err := sql.Open("sqlite3", File)
 
 	if err != nil {
@@ -19,6 +23,16 @@ func (d *Database) Connect() *sql.DB {
 	}
 
 	return db
+}
+
+func createDirectoryIfNotExists(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, 0755)
+		if err != nil {
+			println("Error creating directory", err.Error())
+		}
+	}
+
 }
 
 func CreateOrVerifyTable(c *sql.DB, tableName string, columns string) (err error) {

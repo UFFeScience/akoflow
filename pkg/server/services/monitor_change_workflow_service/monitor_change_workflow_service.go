6cf7de5c2a9f19163f78caaeab50db1b90498e0d
Wesley <me@ovvesley.com>
2024-06-03 21:11:25 -0300

    monitor: adding monitor when completed log

--- pkg/server/services/monitor_change_workflow_service/monitor_change_workflow_service.go
+++ pkg/server/services/monitor_change_workflow_service/monitor_change_workflow_service.go
@@ -2,6 +2,7 @@ package monitor_change_workflow_service
 
 import (
 	"errors"
+	"github.com/ovvesley/akoflow/pkg/server/repository/logs_repository"
 
 	"github.com/ovvesley/akoflow/pkg/server/channel"
 	"github.com/ovvesley/akoflow/pkg/server/connector"
@@ -18,6 +19,7 @@ type MonitorChangeWorkflowService struct {
 	namespace                 string
 	workflowRepository        workflow_repository.IWorkflowRepository
 	activityRepository        activity_repository.IActivityRepository
+	logsRepository            logs_repository.ILogsRepository
 	channelManager            *channel.Manager
 	getPendingWorkflowService *get_pending_workflow_service.GetPendingWorkflowService
 	getWorkflowByStatus       *get_workflow_by_status_service.GetWorkflowByStatusService
@@ -31,6 +33,7 @@ func New() *MonitorChangeWorkflowService {
 		activityRepository:        activity_repository.New(),
 		channelManager:            channel.GetInstance(),
 		getPendingWorkflowService: get_pending_workflow_service.New(),
+		logsRepository:            logs_repository.New(),
 		getWorkflowByStatus:       get_workflow_by_status_service.New(),
 		connector:                 connector.New(),
 	}
@@ -156,6 +159,7 @@ func (m *MonitorChangeWorkflowService) handleVerifyActivityWasFinished(activity 
 
 	if jobResponse.Status.Succeeded == 1 {
 		var _ = m.activityRepository.UpdateStatus(activity.Id, activity_repository.StatusFinished)
+		m.monitorHandleLogs(activity)
 		return activity_repository.StatusFinished
 	}
 
@@ -170,9 +174,25 @@ func (m *MonitorChangeWorkflowService) handleVerifyActivityWasFinished(activity 
 	if jobResponse.Status.Failed >= 1 {
 		println("Activity failed: ", activity.Name)
 		var _ = m.activityRepository.UpdateStatus(activity.Id, activity_repository.StatusFinished)
+		m.monitorHandleLogs(activity)
+
 		return activity_repository.StatusFinished
 	}
 
 	return activity_repository.StatusFinished
 
+}
+
+func (m *MonitorChangeWorkflowService) monitorHandleLogs(activity workflow_activity_entity.WorkflowActivities) {
+	podJob, _ := m.connector.Pod().GetPodByJob(m.namespace, activity.GetNameJob())
+	podName, _ := podJob.GetPodName()
+
+	logs, _ := m.connector.Pod().GetPodLogs(m.namespace, podName)
+
+	var _ = m.logsRepository.Create(logs_repository.ParamsLogsCreate{
+		LogsDatabase: logs_repository.LogsDatabase{
+			ActivityId: activity.Id,
+			Logs:       logs,
+		},
+	})
 }

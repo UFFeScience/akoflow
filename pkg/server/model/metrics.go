c7a7af188cce1292af18a727f88ec914852fd21a
Wesley <me@ovvesley.com>
2025-04-12 21:31:08 +0000

    feat: add Metrics model with database mappings and utility methods

---
+++ pkg/server/model/metrics.go
@@ -0,0 +1,25 @@
+package model
+
+import "github.com/ovvesley/akoflow/pkg/server/utils/utils_model"
+
+type Metrics struct {
+	ID         int    `db:"id" sql:"PRIMARY KEY AUTOINCREMENT"`
+	ActivityID int    `db:"activity_id"`
+	Cpu        string `db:"cpu"`
+	Memory     string `db:"memory"`
+	Window     string `db:"window"`
+	Timestamp  string `db:"timestamp"`
+	CreatedAt  string `db:"created_at" sql:"DEFAULT CURRENT_TIMESTAMP"`
+}
+
+func (Metrics) TableName() string {
+	return "metrics"
+}
+
+func (m Metrics) GetColumns() []string {
+	return utils_model.GenericGetColumns(m)
+}
+
+func (m Metrics) GetPrimaryKey() string {
+	return utils_model.GenericGetPrimaryKey(m)
+}
